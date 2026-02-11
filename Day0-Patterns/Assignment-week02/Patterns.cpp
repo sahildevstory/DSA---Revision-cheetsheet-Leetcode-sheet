fa3142c2de42c6089ea40e700d520f4a62082c2b
sahil codes <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>
2026-02-12 01:38:44 +0530

    updated

--- Day0-Patterns/Assignment-week02/Patterns.cpp
+++ Day0-Patterns/Assignment-week02/Patterns.cpp
@@ -17,24 +17,24 @@ using namespace std;
 //     }
 // }
 
-void InvertedHollowPattern(int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i+1; j <= n; j++)
-        {
-            if (j == i+1 || j == n || i==0)
-            {
-                cout << j ;
-            }
-            else
-            {
-                cout << " ";
-            }
-        }
-        cout << endl;
-    }
-}
+// void InvertedHollowPattern(int n)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = i+1; j <= n; j++)
+//         {
+//             if (j == i+1 || j == n || i==0)
+//             {
+//                 cout << j ;
+//             }
+//             else
+//             {
+//                 cout << " ";
+//             }
+//         }
+//         cout << endl;
+//     }
+// }
 
 int main()
 {
@@ -42,5 +42,5 @@ int main()
     // NumericHollowPattern(5);
 
     // Pattern2: InvertedHollowPattern(5)
-    InvertedHollowPattern(5);
+    // InvertedHollowPattern(5);
 }
